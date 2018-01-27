a40edef41a7d0f86ae60fffee044461e7509b8f2
Chris Westra <cwestra@gmail.com>
2018-01-27 13:17:41 -0500

    Finish chapter 2

--- test/islands_engine_test.exs
+++ test/islands_engine_test.exs
@@ -1,6 +1,7 @@
 defmodule IslandsEngineTest do
   use ExUnit.Case
   doctest IslandsEngine
+  doctest IslandsEngine.Island, import: true
 
   test "greets the world" do
     assert IslandsEngine.hello() == :world
